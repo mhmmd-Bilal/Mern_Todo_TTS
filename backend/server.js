ef45a8a7fb19ee33aec82e957ba4ca7babc7e183
Muhammed Bilal <muhammedbilal6211@gmail.com>
2025-12-04 20:18:06 +0530

    middlewares added

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,31 @@
 import express from "express";
-import connectDb from './config/db.js'
-import dotenv from 'dotenv'
+import connectDb from "./config/db.js";
+import dotenv from "dotenv";
 
 const app = express();
 
-dotenv.config()
+dotenv.config();
 
 let port = process.env.PORT;
 
-connectDb()
+connectDb();
+
+app.use(express.json()); // parses JSON body sent from frontend/postman - data will be available in req.body
+app.use(express.urlencoded({extended : true}))  // used to parse form data (HTML form submission) - data will be available in req.body
 
 // app.method(path , handler)
 
 // http://localhost:3000/
 
-app.get('/' , (req,res)=>{
-    res.send('hello world')
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  res.send("great");
+});
 
 app.listen(port, () => console.log("server started"));
 
-
-
+// Request -> Global Middlewares -> Route Middlewares -> Route handler -> Response

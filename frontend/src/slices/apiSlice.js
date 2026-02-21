bcfec0e19907d0ba55340c90b12e24e3e6ae1111
Muhammed Bilal <130375693+mhmmd-Bilal@users.noreply.github.com>
2026-02-21 20:05:17 +0530

    Change credentials option to 'include' in apiSlice

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://mern-todo-tts.onrender.com",
-    credentials: true,
+    credentials: "include",
   }),
   tagTypes: [],
   endpoints: () => ({}),
